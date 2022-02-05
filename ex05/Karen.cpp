2be3069ef3d98dc21f4c27c88ce50e8716f38243
dutenrapha <dutenrapha@homail.com>
2022-02-05 17:50:39 +0100

    ex05 working

--- ex05/Karen.cpp
+++ ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: rdutenke <rdutenke@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 17:03:05 by rdutenke          #+#    #+#             */
-/*   Updated: 2022/02/05 17:39:27 by rdutenke         ###   ########.fr       */
+/*   Updated: 2022/02/05 17:48:13 by rdutenke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,22 @@ Karen::~Karen(void)
 
 void Karen::debug(void)
 {
-	std::cout << "\"DEBUG\" level: Debug messages contain contextual information. They are mostly used for problem diagnosis. Example: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void Karen::info(void)
 {
-	std::cout << "\"INFO\" level: These messages contain extensive information. They are helpful for tracing program execution in a production environment. Example: \"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\"" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Karen::warning(void)
 {
-	std::cout << "\"WARNING\" level: Warning messages indicate a potential issue in the system. However, it can be handled or ignored. Example: \"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\"" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Karen::error(void)
 {
-	std::cout << "\"ERROR\" level: These messages indicate an unrecoverable error has occurred. This is usually a critical issue that requires manual intervention. Example: \"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Karen::complain(std::string level)
